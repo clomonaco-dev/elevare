c2f040fd69aecb20e3ae06aa92aec9cd441a8fa7
clomonaco <c.lomonaco@clmautomation.com>
2026-05-04 14:08:51 +0200

    aggiunto posizionamento del banner

---
+++ src/js/iubenda.js
@@ -0,0 +1,15 @@
+window._iub = window._iub || [];
+
+window._iub.csConfiguration = {
+  askConsentAtCookiePolicyUpdate: true,
+  floatingPreferencesButtonDisplay: "bottom-right",
+  perPurposeConsent: true,
+  siteId: 0, // NON serve con widget, puoi lasciarlo 0
+  cookiePolicyId: 0, // NON serve con widget
+  banner: {
+    position: "bottom",
+    acceptButtonDisplay: true,
+    rejectButtonDisplay: true,
+    customizeButtonDisplay: true
+  }
+};
