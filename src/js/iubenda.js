3599564afe675b2930c6633e808b2fe4fdb0d09c
clomonaco <c.lomonaco@clmautomation.com>
2026-05-04 14:39:33 +0200

    tolto clodfire

--- src/js/iubenda.js
+++ src/js/iubenda.js
@@ -2,10 +2,9 @@ window._iub = window._iub || [];
 
 window._iub.csConfiguration = {
   askConsentAtCookiePolicyUpdate: true,
-  floatingPreferencesButtonDisplay: "bottom-right",
+  floatingPreferencesButtonDisplay: "bottom-left",
   perPurposeConsent: true,
-  siteId: 0, // NON serve con widget, puoi lasciarlo 0
-  cookiePolicyId: 0, // NON serve con widget
+
   banner: {
     position: "bottom",
     acceptButtonDisplay: true,
